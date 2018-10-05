147c37faf7f93fe0d97d0aaed004c8ca2c40836e
mehdithe <mehdi.cheracher@gmail.com>
2018-10-05 12:25:34 +0100

    added files exercices

---
+++ wip/files/index.js
@@ -0,0 +1,18 @@
+const FileReader = require("./file-reader.js");
+
+let reader = new FileReader("../..");
+
+/**
+ * list will return a json representation of the directory that was used to construct
+ * the FileReader object
+ */
+console.log(reader.list());
+
+/**
+ * filter the directory file content based on the given param function
+ */
+console.log(
+  reader.listFiltered(file => {
+    return true;
+  })
+);
