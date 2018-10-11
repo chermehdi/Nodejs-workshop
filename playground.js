9bd840a64da586526452422dc3ec1bb494b05d0e
mehdithe <mehdi.cheracher@gmail.com>
2018-10-11 16:30:39 +0100

    wip

--- playground.js
+++ playground.js
@@ -1,2 +1,10 @@
 // run this file using node playground.js
 console.log("This is a test");
+
+const { spawn } = require("child_process");
+
+const command = spawn("ls", ["-alh"]);
+
+command.stdout.on("data", data => {
+  console.log("command executed ", data.toString());
+});
